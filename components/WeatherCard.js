53d09be33ce177d1489157ed94e0296fe3ee9147
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-11-13 14:16:24 +0000

    part1  is done

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -2,19 +2,20 @@ import React from "react";
 import sunny from "../assets/Sunny.svg"
 
 
-function WeatherCard() {
+function WeatherCard({city}) {
   return (
     <div className = "card">
         <div className = "img-container">
             <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
-            <h3 className="card-title">CITY DATA HERE</h3>
-            <h5 className="card-text">TEMPERATURE DATA HERE</h5>
-            <h5 className="card-text">FORECAST DATA HERE</h5>
+            <h3 className="card-title">{city.city}</h3>
+            <h5 className="card-text">{city.temperature}</h5>
+            <h5 className="card-text">{city.forecast}</h5>
         </div>
     </div>
   );
 };
 
 // Export the WeatherCard
+module.exports = WeatherCard;
